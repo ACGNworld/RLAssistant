e6fbf85550b4f27fe8918efd30243f112bae4e7f
FJJ <50703029@qq.com>
2025-03-13 22:48:11 +0800

    test and fix some bugs out of date

--- example/sb3_ppo_example/ppo/main.py
+++ example/sb3_ppo_example/ppo/main.py
@@ -1,17 +1,18 @@
-import gym
+import gymnasium as gym
 
 from ppo import PPO
 import argparse
 from RLA.rla_argparser import arg_parser_postprocess
-from RLA.easy_log.tester import exp_manager
-from RLA.easy_log import logger
+from RLA import exp_manager
+from RLA import logger
 
 def mujoco_arg_parser():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', help='environment ID', type=str, default='CartPole-v1')
     parser.add_argument('--policy_type',  type=str, default='MlpPolicy')
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
-    parser.add_argument('--total_timesteps', type=int, default=int(1e5))
+    parser.add_argument('--total_timesteps', type=int, default=int(1e4))
+    parser.add_argument('--render_mode', type=str, default="human")
     # [RLA] add RLA parameters
     parser = arg_parser_postprocess(parser)
     return parser
@@ -22,7 +23,7 @@ args = mujoco_arg_parser().parse_args()
 task_name = 'demo_task'
 exp_manager.set_hyper_param(**vars(args))
 exp_manager.add_record_param(["info", "seed", 'env'])
-exp_manager.configure(task_name, rla_config='../rla_config.yaml', data_root='../')
+exp_manager.configure(task_name, rla_config='./rla_config.yaml', data_root='./')
 exp_manager.log_files_gen()
 exp_manager.print_args()
 
@@ -38,12 +39,12 @@ model._custom_logger = True
 
 model.learn(total_timesteps=args.total_timesteps)
 
-obs = env.reset()
+obs, _ = env.reset()
 for i in range(1000):
-    action, _states = model.predict(obs, deterministic=True)
-    obs, reward, done, info = env.step(action)
+    action, _ = model.predict(obs, deterministic=True)  # Pass only `obs`, not the tuple
+    obs, reward, terminated, truncated, info = env.step(action)
     env.render()
-    if done:
-      obs = env.reset()
+    if terminated or truncated:
+        obs, _ = env.reset()  # Reset and unpack only `obs`
 
 env.close()
